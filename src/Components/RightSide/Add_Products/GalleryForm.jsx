0d5e78b88e8415076693b3a53a51e9b2e2601723
KinCode36 <nickyrock2390@gmail.com>
2026-08-21 15:03:33 +0530

    Add Upload Image Section and Submit Button

--- src/Components/RightSide/Add_Products/GalleryForm.jsx
+++ src/Components/RightSide/Add_Products/GalleryForm.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const GalleryForm = () => {
     return (
         <form action="">
+            {/* gallery Category  */}
             <label className="block mb-2 font-semibold">
                 Gallery Category{" "}
                 <span className="text-red-500">*</span>
             </label>
+            {/* Select Category */}
             <select className='w-full border border-gray-300 rounded-lg p-3 mb-5' required>
                 <option value="">Select Category</option>
                 <option value="Wedding">Wedding</option>
@@ -17,11 +19,13 @@ const GalleryForm = () => {
             </select>
 
             <label className='block mb-2 font-semibold'>
-                 Upload Images{" "}
-            <span className='text-red-500'>*</span>
+                Upload Images{" "}
+                <span className='text-red-500'>*</span>
             </label>
-            <input type="file" accept="image/*" multiple className='w-full border border-gary' />
-           
+            {/* Images */}
+            <input type="file" accept="image/*" multiple className='w-full border border-gary-300 rounded-lg p-3' />
+            {/* Submit Button */}
+            <button className='w-[97%] active:scale-95 px-4 py-2 bg-blue-600 text-white rounded-md transition-all duration-150 font-semibold cursor-pointer mt-5'>Upload Images</button>
         </form>
     )
 }
