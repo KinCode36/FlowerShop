66de57dc287a2ef7ffefa6b92692d6fc312bc0ea
KinCode36 <nickyrock2390@gmail.com>
2026-09-03 18:17:36 +0530

    Saprate elements of the create page into differ differ files

--- src/Components/Login/CreateAccount/CreateAccount.jsx
+++ src/Components/Login/CreateAccount/CreateAccount.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import CreateAcountTop from './CreateAccountTop/CreateAcountTop'
 import CRForm from './CRForm/CRForm'
 import CreateAccountFooter from './CreateAccountFooter/CreateAccountFooter'
+import OrLine from './OrLine/OrLine'
 const CreateAccount = () => {
 
 
@@ -16,16 +17,10 @@ const CreateAccount = () => {
         <main className='flex flex-1 w-full justify-center items-center px-4 py-8 sm:px-6 md:px-10 lg:px-12 xl:px-16 2xl:px-20'>
           <div className='bg-white w-full max-w-[480px] rounded-2xl border border-[#dfded9] p-5 shadow-[0_12px_35px_rbga(0,0,0,0,0.86)] sm:p-7 md:p-8 lg:p-9'>
 
-            <CreateAcountTop/>
-
-          <CRForm/>
-
-            <div className='flex items-center gap-3 my-6'>
-              <div className='h-px flex flex-1 bg-[#e2e1dc]'></div>
-              <div className='text-xs text-[#999994]'>OR</div>
-              <div className='h-px flex flex-1 bg-[#e2e1dc]'></div>
-            </div>
-           <CreateAccountFooter/>
+            <CreateAcountTop />
+            <CRForm />
+            <OrLine />
+            <CreateAccountFooter />
           </div>
         </main>
       </div>
---
+++ src/Components/Login/CreateAccount/OrLine/OrLine.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OrLine = () => {
+    return (
+        <div className='flex items-center gap-3 my-6'>
+            <div className='h-px flex flex-1 bg-[#e2e1dc]'></div>
+            <div className='text-xs text-[#999994]'>OR</div>
+            <div className='h-px flex flex-1 bg-[#e2e1dc]'></div>
+        </div>
+    )
+}
+
+export default OrLine
