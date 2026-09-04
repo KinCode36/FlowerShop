162645c54f5f794bff8c7338e82605c72a47f5a1
KinCode36 <nickyrock2390@gmail.com>
2026-09-04 15:45:27 +0530

    Add logout button to logout from admin panel

--- src/Components/NavBar/RightNav.jsx
+++ src/Components/NavBar/RightNav.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { logoutUser } from '../../utils/localStrage'
+import { useNavigate } from 'react-router-dom'
 
 const RightNav = () => {
+const navigate =  useNavigate()
+  const handleLogout = () => {
+    logoutUser()
+    navigate("/login")
+  }
+
   return (
     
 
@@ -10,8 +18,8 @@ const RightNav = () => {
           <span className="absolute top-1.5 right-1.5 w-2 h-2 rounded-full bg-[#C8897A] border border-white"></span>
         </button>
 
-        <button className="w-9 h-9 rounded-lg border border-[#E0DDD6] flex items-center justify-center hover:bg-[#F4F3EF]">
-          👤
+        <button onClick={handleLogout} className="p-1.5 font-bold hover:text-white transition-all duration-150 active:scale-95 rounded-lg border border-[#E0DDD6] flex items-center justify-center hover:bg-red-800">
+          Logout
         </button>
       </div>
     
